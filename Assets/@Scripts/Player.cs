90dd8a916575b7652161e210fcfed4495e16b4c0
simrok <ynna3784@gmail.com>
2026-09-18 18:15:03 +0900

    dev: 플레이어 움직임 구현

--- Assets/@Scripts/Player.cs
+++ Assets/@Scripts/Player.cs
@@ -1,22 +1,49 @@
 using UnityEngine;
+using System.Collections;
 [RequireComponent(typeof(Rigidbody))] 
 public class Player : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Animator animator;
+    private PlayerMovement playerMovement;
     private Enemy enemy;
 
     private float hp { get; set; }
-    
-    private float attackSpeed { get; set; }
-    private float normalAttackDamage { get; set; }
+
+    private float attackSpeed;
+    private float normalAttackDamage;
 
     // 관통력, .. 등 추가
 
     private bool isAttack;  // 공격 중
+    private bool isSkilled; // 스킬 시전 중
+
+    [Header("구르기 값")]
+    [SerializeField] private float rollDuration = 0.4f; // 지속시간 0.35~0.45
+    [SerializeField] private float rollMoveSpeed = 60f; // 구르기 이동 속도(거리 = 속력 * 시간)
+    private float invincibleRatio = 0.65f;  // 무적상태는 전체 중 앞 65% 구간 지속
+    [SerializeField] private AnimationCurve rollSpeedCurve;
+    private float recoveryTime = 0.1f;    // 재입력 불가 구간 0.1~0.15초
+    private bool isInvincible; // 무적 상태인지
+    private bool isRoll;
+    // 회피 도중 방향 전환되지 않게 하기 위한 변수
+    Vector3 rollVec;
+    private float elapsed;
+
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
         isAttack = false;
+        isInvincible = false;
         hp = 100f;
         normalAttackDamage = 10f;
+    }
+
+    private void FixedUpdate()
+    {
+ 
     }
 
     // 일반 공격
@@ -29,13 +56,9 @@ public class Player : MonoBehaviour
         Collider[] colls = Physics.OverlapSphere(transform.position, 0.7f);
         foreach (Collider coll in colls)
         {
-            Debug.Log(coll.name);
             enemy = coll.GetComponentInParent<Enemy>();
-            if (enemy == null) Debug.Log("enemy가 비었다");
             if (enemy != null)
             {
-                Debug.Log("테스트다옹");
-                Debug.Log(enemy.hp);
                 enemy.GetDamage(normalAttackDamage);
             }
         }
@@ -52,12 +75,44 @@ public class Player : MonoBehaviour
     }
 
     // 앞으로 구르기
-    public void playerRoll()
+    public void Roll()
     {
-        // 현재 바라보고 있는 방향으로 전진하면서 구르는 애니메이션
-
+        isRoll = true;
+        playerMovement.canMove = false;
+        StartCoroutine(RollRoutine());
     }
 
+    private IEnumerator RollRoutine()
+    {
+        elapsed = 0f;
+        rollVec = transform.forward;
+        Vector3 startPos = transform.position;
+
+        while (elapsed <= rollDuration)
+        {
+            // 무적 프레임은 앞쪽 60~70%. 
+            isInvincible = elapsed < rollDuration * invincibleRatio;
+            // 현재 바라보고 있는 방향으로 전진하면서 구르는 애니메이션
+            // 지속시간 0.35~0.45초.  이동거리는 캐릭터 크기의 2~3배 정도
+            float speed = rollMoveSpeed * rollSpeedCurve.Evaluate(elapsed / rollDuration);
+            Debug.Log($"테스트: speed={speed}, rollMoveSpeed={rollMoveSpeed}");
+            playerMovement.ForceMove(rollVec, speed);
+            elapsed += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log("구르는 중");
+        Debug.Log(Vector3.Distance(startPos, transform.position));
+
+        isInvincible = false;
+        isRoll = false;
+        playerMovement.canMove = true;
+
+        // 구르기 종료 후 아주 짧은 재입력 불가 구간 0.1~0.15초
+        yield return new WaitForSeconds(recoveryTime);
+
+        // 구르기 사용시 진행 중이던 콤보 카운터는 리셋
+        isAttack = false;
+    }
     // 구르기 콤보
 
     // 스킬사용
@@ -84,42 +139,51 @@ public class Player : MonoBehaviour
         bComboExist = true;
     }
 
-
-
     private void Update()
     {
+        animator.SetFloat("Blend", playerMovement.Dir != Vector3.zero ? 1f : 0f);
         // D: 기본 공격
-        if (Input.GetKeyDown(KeyCode.D) && !isAttack)
+        if (Input.GetKeyDown(KeyCode.D) && !isAttack)   // 공격 중이 아니면 공격 가능
         {
             isAttack = true;
             NormalAttack();
 
             isAttack = false;
+            Debug.Log("기본 공격 후 isAttack 상태: " + isAttack);
         }
         // S: 구르기
-        if (Input.GetKeyDown(KeyCode.S) && !isAttack)
+        if (Input.GetKeyDown(KeyCode.S) && !isAttack && !isRoll)
         {
             isAttack = true;
-            
+            animator.SetTrigger("Roll");
+            Roll();
+
+            Debug.Log("구르기 후 isAttack 상태: " + isAttack);
         }
 
         // A: 콤보
-        if(Input.GetKeyDown(KeyCode.A) && !isAttack)
+        if (Input.GetKeyDown(KeyCode.A) && !isAttack)
         {
             isAttack = true;
 
         }
 
         // W: 스킬1 사용
-        if(Input.GetKeyDown(KeyCode.C) && !isAttack)
+        if (Input.GetKeyDown(KeyCode.C) && !isAttack)
         {
             isAttack = true;
-
         }
 
         // E: 스킬2 사용
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+        }
 
         // R: 상호 작용
     }
 
 }
+
+
+
+
--- Assets/@Scripts/PlayerMovement.cs
+++ Assets/@Scripts/PlayerMovement.cs
@@ -3,22 +3,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 3f;
+    public float moveSpeed;
+    public float rotationSpeed;
 
-    Rigidbody rb;
-    Vector3 moveInput;
+    private Rigidbody rb;
 
-    private void Awake() => rb = GetComponent<Rigidbody>();
+    private Vector3 dir;
+    public Vector3 Dir => dir;
+
+    public bool canMove;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = 6f;
+        rotationSpeed = 300f;
+        canMove = true;
+    }
 
     private void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        moveInput = new Vector3(h, 0f, v).normalized;
+        dir = new Vector3(h, 0f, v).normalized;
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        if (canMove)
+        {
+            rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+        }
+
+        if (dir != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            Quaternion nextRotation = Quaternion.RotateTowards(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            rb.MoveRotation(nextRotation);
+        }
+
+    }
+
+    public void ForceMove(Vector3 direcion, float speed)
+    {
+        rb.MovePosition(rb.position + direcion * speed * Time.fixedDeltaTime);
     }
 }
